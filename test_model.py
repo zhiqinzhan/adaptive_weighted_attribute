120d9c3000d877e109b0eba832b884789295200b
ZhiQin Zhan <zqzhan14@fudan.edu.cn>
2018-05-24 23:47:42 +0800

    testing

--- test_model.py
+++ test_model.py
@@ -28,7 +28,7 @@ a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
 mean = caffe.io.blobproto_to_array(a)[0]
 
 prototxt_path = 'deploy_resnet.prototxt'
-model_path = 'model/CrossEntropy_saved_weighted__iter_130000.caffemodel'
+model_path = 'model/archive/joint_Euclidean_w1_iter_130000.caffemodel'
 net = caffe.Net(prototxt_path, model_path, caffe.TEST)
 
 target_height = 224
