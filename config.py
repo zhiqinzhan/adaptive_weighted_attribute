b0b572e812d1fe91598eaba72c015bb6ef206631
Zhiqin Zhan <zqzhan14@fudan.edu.cn>
2018-07-27 21:53:05 +0800

    basic_CE

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import yaml
 import os
 
 dataset_name = "PA-100K"
-model_name = "joint_CE"
-train_gpu_id = 0
+model_name = "basic_CE"
+train_gpu_id = 1
 test_gpu_id = 3
 
 with open(os.path.join("model", dataset_name, model_name, "config.yml"), "r") as f:
