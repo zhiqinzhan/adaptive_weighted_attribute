50207dd1b50a0c19866e01282093856ad99f7dd4
ZhiQin Zhan <zqzhan14@fudan.edu.cn>
2018-09-03 23:00:22 +0800

    balanced_loss_layer

--- balanced_loss_layer.py
+++ balanced_loss_layer.py
@@ -9,16 +9,15 @@ majority_ratio = np.asarray([config.pos_ratio[i] if majority[i] else 1 - config.
     for i in range(attribute_size)], dtype=np.float32)
 majority_drop_rate = 2 - 1 / majority_ratio
 
-print attribute_size
-print config.pos_ratio
-print majority
-print majority_ratio
-print majority_drop_rate
-
 def dropGradient(bottom):
     batch_size = bottom[0].shape[0]
     assert(attribute_size == bottom[0].shape[1])
     y = bottom[1].data > 0 # ground-truth
+    r = np.random.rand(batch_size, attribute_size)
+    for b in range(batch_size):
+        for a in range(attribute_size):
+            if y[b][a] == majority[a] and r[b][a] < majority_drop_rate[a]:
+                bottom[0].diff[b][a] = 0
 
 class balancedSigmoidCrossEntropyLossLayer(
         python_loss_layer.SigmoidCrossEntropyLossLayer
